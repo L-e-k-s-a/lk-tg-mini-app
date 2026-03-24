e8c6512f89697883fd618ac48548529581d448d1
deadsxnpai <deadsxnpai@gmail.com>
2026-03-24 16:11:51 +0300

    fix: added me

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,23 @@
 import { useTgAuth } from '@/features';
-import { Typography } from '@/shared';
+import { Loader, Typography } from '@/shared';
+import { useMe } from '@/shared/api';
 import { MainLayout } from '@/shared/layouts';
 import { detectPlatform } from '@/shared/lib/platform/get-platform';
+import { ErrorView } from '@/widgets/error-view';
 import { retrieveLaunchParams } from '@tma.js/sdk';
 import React from 'react';
 
 export default function HomeScreenTab() {
 	const { tgUser } = useTgAuth();
 	const params = retrieveLaunchParams();
-	// const { data, loading, error } = useMe();
+	const { data, loading, error } = useMe();
 
-	// if (loading) return <Loader />;
-	// if (error) return <ErrorView error={error} />;
+	if (loading) return <Loader />;
+	if (error) return <ErrorView error={error} />;
 	const platform = detectPlatform();
 	return (
 		<MainLayout>
-			{/* <Typography variant='body'>{JSON.stringify(data, null, 2)}</Typography> */}
+			<Typography variant='body'>{JSON.stringify(data, null, 2)}</Typography>
 			<Typography variant='body'>
 				{JSON.stringify(platform, null, 2)}
 			</Typography>
