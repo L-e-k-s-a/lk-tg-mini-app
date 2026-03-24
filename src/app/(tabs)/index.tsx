4f0ad2f4cfc962abfbb7aeca9e96cf22e00638b4
deadsxnpai <deadsxnpai@gmail.com>
2026-03-24 16:46:33 +0300

    fix: removed some files

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,11 @@
-import { useTgAuth } from '@/features';
 import { Loader, Typography } from '@/shared';
 import { useMe } from '@/shared/api';
 import { MainLayout } from '@/shared/layouts';
 import { detectPlatform } from '@/shared/lib/platform/get-platform';
 import { ErrorView } from '@/widgets/error-view';
-import { retrieveLaunchParams } from '@tma.js/sdk';
 import React from 'react';
 
 export default function HomeScreenTab() {
-	const { tgUser } = useTgAuth();
-	const params = retrieveLaunchParams();
 	const { data, loading, error } = useMe();
 
 	if (loading) return <Loader />;
@@ -21,8 +17,6 @@ export default function HomeScreenTab() {
 			<Typography variant='body'>
 				{JSON.stringify(platform, null, 2)}
 			</Typography>
-			<Typography variant='body'>{JSON.stringify(tgUser, null, 2)}</Typography>
-			<Typography variant='body'>{JSON.stringify(params, null, 2)}</Typography>
 		</MainLayout>
 	);
 }
