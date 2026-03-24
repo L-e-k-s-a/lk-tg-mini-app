155ee0f2528842d1401b6cfc926bd4502a3dc6b5
deadsxnpai <deadsxnpai@gmail.com>
2026-03-24 16:03:14 +0300

    feat: added params

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,10 +2,12 @@ import { useTgAuth } from '@/features';
 import { Typography } from '@/shared';
 import { MainLayout } from '@/shared/layouts';
 import { detectPlatform } from '@/shared/lib/platform/get-platform';
+import { retrieveLaunchParams } from '@tma.js/sdk';
 import React from 'react';
 
 export default function HomeScreenTab() {
 	const { tgUser } = useTgAuth();
+	const params = retrieveLaunchParams();
 	// const { data, loading, error } = useMe();
 
 	// if (loading) return <Loader />;
@@ -18,6 +20,7 @@ export default function HomeScreenTab() {
 				{JSON.stringify(platform, null, 2)}
 			</Typography>
 			<Typography variant='body'>{JSON.stringify(tgUser, null, 2)}</Typography>
+			<Typography variant='body'>{JSON.stringify(params, null, 2)}</Typography>
 		</MainLayout>
 	);
 }
