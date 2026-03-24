da245c536681ea7990bce7ecff672ad1f2d5199e
deadsxnpai <deadsxnpai@gmail.com>
2026-03-24 14:32:19 +0300

    fix: hotfix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,28 @@
-import { Loader, Typography } from '@/shared';
+import { useTgAuth } from '@/features';
+import { Container, Loader, Typography } from '@/shared';
 import { useMe } from '@/shared/api';
 import { MainLayout } from '@/shared/layouts';
 import { ErrorView } from '@/widgets/error-view';
 import React from 'react';
 
 export default function HomeScreenTab() {
-	const { data, loading, error } = useMe();
+	const { tgUser } = useTgAuth();
+	// const { data, loading, error } = useMe();
 
-	if (loading) return <Loader />;
-	if (error) return <ErrorView error={error} />;
+	// if (loading) return <Loader />;
+	// if (error) return <ErrorView error={error} />;
 
 	return (
 		<MainLayout>
-			<Typography variant='body'>{JSON.stringify(data, null, 2)}</Typography>
+			<Container>
+				{/* <Typography variant='body'>{JSON.stringify(data, null, 2)}</Typography> */}
+				<></>
+			</Container>
+			<Container>
+				<Typography variant='body'>
+					{JSON.stringify(tgUser, null, 2)}
+				</Typography>
+			</Container>
 		</MainLayout>
 	);
 }
