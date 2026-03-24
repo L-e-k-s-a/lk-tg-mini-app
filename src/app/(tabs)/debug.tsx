31d8e318817247da27db8d10a8c7d9aa25b3f6ae
deadsxnpai <deadsxnpai@gmail.com>
2026-03-24 09:20:13 +0300

    fix: hotfix

--- src/app/(tabs)/debug.tsx
+++ src/app/(tabs)/debug.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function DebugTgInfo() {
-	const { tgInitialized, tgUser, isLoading, rawInitData } = useTgAuth();
+	const { tgUser, isLoading, rawInitData } = useTgAuth();
 
 	if (isLoading) {
 		return (
