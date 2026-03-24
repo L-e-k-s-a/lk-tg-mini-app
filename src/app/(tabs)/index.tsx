392410dfac351484296449efc4097f92816e5e09
deadsxnpai <deadsxnpai@gmail.com>
2026-03-24 16:46:47 +0300

    fix: removed platrfomr

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,6 @@
 import { Loader, Typography } from '@/shared';
 import { useMe } from '@/shared/api';
 import { MainLayout } from '@/shared/layouts';
-import { detectPlatform } from '@/shared/lib/platform/get-platform';
 import { ErrorView } from '@/widgets/error-view';
 import React from 'react';
 
@@ -10,13 +9,10 @@ export default function HomeScreenTab() {
 
 	if (loading) return <Loader />;
 	if (error) return <ErrorView error={error} />;
-	const platform = detectPlatform();
+
 	return (
 		<MainLayout>
 			<Typography variant='body'>{JSON.stringify(data, null, 2)}</Typography>
-			<Typography variant='body'>
-				{JSON.stringify(platform, null, 2)}
-			</Typography>
 		</MainLayout>
 	);
 }
